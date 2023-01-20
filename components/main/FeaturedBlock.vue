a01320dbd396f0dd6526e3f50fc27e03fde57bca
innaos07 <samosudova07@gmail.com>
2023-01-20 15:14:37 +0100

    improved FeaturedBlock

--- components/main/FeaturedBlock.vue
+++ components/main/FeaturedBlock.vue
@@ -79,13 +79,13 @@ export default {
     margin-top: -114px;
     margin-right: 20px;
     margin-left: 20px;
-    filter: drop-shadow(0px 163px 80px rgba(0, 0, 0, 0.04))
-      drop-shadow(0px 105.648px 46.8519px rgba(0, 0, 0, 0.0303704))
-      drop-shadow(0px 62.7852px 25.4815px rgba(0, 0, 0, 0.0242963))
-      drop-shadow(0px 32.6px 13px rgba(0, 0, 0, 0.02))
-      drop-shadow(0px 13.2815px 6.51852px rgba(0, 0, 0, 0.0157037))
-      drop-shadow(0px 3.01852px 3.14815px rgba(0, 0, 0, 0.00962963))
-      drop-shadow(48px 58px 44px rgba(0, 0, 0, 0.09));
+    box-shadow: 0px 163px 80px rgba(0, 0, 0, 0.04),
+                0px 105.648px 46.8519px rgba(0, 0, 0, 0.0303704),
+                0px 62.7852px 25.4815px rgba(0, 0, 0, 0.0242963),
+                0px 32.6px 13px rgba(0, 0, 0, 0.02),
+                0px 13.2815px 6.51852px rgba(0, 0, 0, 0.0157037),
+                0px 3.01852px 3.14815px rgba(0, 0, 0, 0.00962963),
+                48px 58px 44px rgba(0, 0, 0, 0.09);
   }
 
   .info-featured {
